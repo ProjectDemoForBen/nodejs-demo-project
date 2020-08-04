bd4c623bf6c608266f57e8f86036f219f9255e53
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-08-03 21:44:25 -0400

    Improve styling

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require("path");
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const rootDir = require("./utils/path");
+const rootDir = require('./utils/path');
 
 // initializes express object that handles the incoming requests
 const app = express();
@@ -12,32 +12,29 @@ const app = express();
 // by default, express doesnt parse the request body, so add Parser Middleware
 // text parser
 // npm install --save body-parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // middleware that allows us to serve static files (forwarded directly from the filesystem)
 // gives read access to the root path
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 // can register multiple static folders, it will go through all of them until it gets a match
-
 
 // middlewares that should match all request should be put first
 // eg
 app.use('/', (req, res, next) => {
     // console.log("always run");
     next();
-})
+});
 
 // para acceder al route de admin es necesario que
 // tenga como prepend /admin
 // ej. /admin/add-product
-app.use('/admin', adminRoutes);   // dentro de adminRoutes, no es necesario saber que tiene como prepend /admin
+app.use('/admin', adminRoutes); // dentro de adminRoutes, no es necesario saber que tiene como prepend /admin
 
 app.use(shopRoutes);
 
-
 // requests goes from top to bottom, so if it reaches this path, return an 404 page
 app.use((req, res, next) => {
-
     res.status(404);
     res.sendFile(path.join(rootDir, 'views', '404.html'));
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,25 @@
-const path = require("path");
-const express = require("express");
+const path = require('path')
+const express = require('express')
 
-const rootDir = require("../utils/path");
+const rootDir = require('../utils/path')
 
 // like a mini express which is plug to the expressjs
 // works like app
-const router = express.Router();
-
+const router = express.Router()
 
 // default path is '/'
 // use: it is a startsWith so, the specific address should be put first
 // get,... : the url should match the path
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir,'views','add-product.html'));
-
-});
-
-router.post('/product', (req, res, next) => {
-    // req.body is added by ExpressJS
-
-    const title = req.body.title;
-    console.log(title);
-    res.redirect('/');
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
-module.exports = router;
+router.post('/add-product', (req, res, next) => {
+    // req.body is added by ExpressJS
+
+    const title = req.body.title
+    console.log(title)
+    res.redirect('/')
+})
+
+module.exports = router
