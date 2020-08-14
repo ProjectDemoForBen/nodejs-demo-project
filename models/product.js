8c66d04a0c99a7ce8d287e94ae653869c03b5532
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-08-13 21:24:57 -0400

    Product model using mongoose

--- models/product.js
+++ models/product.js
@@ -1,65 +1,26 @@
-const mongoDb = require('mongodb');
-const getDb = require('../utils/database').getDb;
+const mongoose = require('mongoose');
 
-const collection = 'products';
+const Schema = mongoose.Schema;
 
-class Product {
-    constructor(id, title, price, imageUrl, description, userId) {
-        this._id = id ? mongoDb.ObjectID(id) : null;
-        this.title = title;
-        this.price = price;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.userId = userId; // user who added the Product
-    }
+// even though nosql is schemaless there is always a structure, mongoose gives the flexibility to access the
+// properties which are optional (by default)
+// _id: will be added automatically
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+});
 
-    save() {
-        const db = getDb();
-        const dbCollection = db.collection(collection);
-        if (this._id) {
-            return dbCollection.updateOne(
-                {
-                    _id: this._id,
-                },
-                {
-                    $set: {
-                        title: this.title,
-                        price: this.price,
-                        imageUrl: this.imageUrl,
-                        description: this.description,
-                    },
-                }
-            );
-        } else {
-            return dbCollection.insertOne(this);
-        }
-    }
-
-    static fetchAll() {
-        const db = getDb();
-
-        // .find({title: 'book'}) // find products that has book as a title
-
-        return db
-            .collection(collection)
-            .find() // returns a cursor (uses pagination)
-            .toArray(); // return all elements (promise);
-    }
-
-    static findById(id) {
-        const db = getDb();
-        return db
-            .collection(collection)
-            .find({ _id: new mongoDb.ObjectID(id) }) // because the _id is stored as an ObjectId
-            .next();
-    }
-
-    static deleteById(id) {
-        const db = getDb();
-        return db
-            .collection(collection)
-            .deleteOne({ _id: new mongoDb.ObjectID(id) });
-    }
-}
-
-module.exports = Product;
+module.exports = mongoose.model('Product', productSchema);
