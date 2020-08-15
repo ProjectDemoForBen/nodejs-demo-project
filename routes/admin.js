799c1e1b763084926cac2b2bfc7648361a01950a
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-08-15 19:42:28 -0400

    Protecting routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const isAuth = require('../middleware/is-auth');
+
 // like a mini express which is plug to the expressjs
 // works like app
 const router = express.Router();
 
 const adminController = require('../controllers/admin');
+
+// all the other routes will require to be authenticated
+router.use(isAuth);
 
 // default path is '/'
 // use: it is a startsWith so, the specific address should be put first
--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const shopController = require('../controllers/shop');
 
+const isAuth = require('../middleware/is-auth');
+
 // like a mini express which is plug to the expressjs
 // works like app
 const router = express.Router();
@@ -12,12 +14,12 @@ router.get('/products', shopController.getProducts);
 // put more specific path first
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCart);
-router.post('/cart-remove-item', shopController.postRemoveItemFromCart);
+router.get('/cart', isAuth, shopController.getCart);
+router.post('/cart', isAuth, shopController.postCart);
+router.post('/cart-remove-item', isAuth, shopController.postRemoveItemFromCart);
 
 // router.get('/checkout', shopController.getCheckout);
-router.get('/orders', shopController.getOrders);
-router.post('/create-order', shopController.postCreateOrder);
+router.get('/orders', isAuth, shopController.getOrders);
+router.post('/create-order', isAuth, shopController.postCreateOrder);
 
 module.exports = router;
