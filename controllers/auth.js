e1233e5f228df2cfefd0192b96403fc4ad4279ee
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-08-17 23:27:25 -0400

    Add reset password via email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
@@ -116,6 +118,127 @@ exports.postSignup = (req, res, next) => {
                 subject: 'Titulo',
                 html: '<h1>Successfully signed up</h1>',
             });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+exports.getReset = (req, res, next) => {
+    const error = req.flash('error');
+    let message = null;
+    if (error.length > 0) {
+        message = error[0];
+    }
+
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset Password',
+        errorMessage: message,
+    });
+};
+
+exports.postReset = (req, res, next) => {
+    const { email } = req.body;
+
+    crypto.randomBytes(32, (err, buf) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/reset');
+        }
+        const token = buf.toString('hex');
+
+        User.findOne({
+            email: email,
+        })
+            .then((user) => {
+                if (!user) {
+                    req.flash('error', 'No email');
+                    return res.redirect('/reset');
+                }
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now() + 300000;
+                return user.save();
+            })
+            .then((user) => {
+                res.redirect('/');
+                return transporter.sendMail({
+                    to: user.email,
+                    from: 'chelo2252the@gmail.com',
+                    subject: 'Reset Password',
+                    html: `
+                    <p>Someone just requested to change your account's credentials. If this was you, click on the link below to reset them.</p>
+                    <a href="http://localhost:3000/reset-password/${token}">Link to reset credentials</a>
+                    <p>This link will expire within 5 minutes.</p>
+                    <p>If you don't want to reset your credentials, just ignore this message and nothing will be changed.</p>
+                    `,
+                });
+            });
+    });
+};
+
+exports.getResetPassword = (req, res, next) => {
+    const error = req.flash('error');
+    let message = null;
+    if (error.length > 0) {
+        message = error[0];
+    }
+
+    const { token } = req.params;
+
+    User.findOne({
+        resetToken: token,
+        resetTokenExpiration: {
+            $gt: Date.now(),
+        },
+    })
+        .then((user) => {
+            if (!user) {
+                return res.redirect('/');
+            }
+            res.render('auth/reset-password', {
+                path: '/reset-password',
+                pageTitle: 'Reset Password',
+                errorMessage: message,
+                userId: user._id,
+                passwordToken: token,
+            });
+        })
+        .catch((error1) => {
+            console.log(error1);
+        });
+};
+
+exports.postResetPassword = (req, res, next) => {
+    const { userId, password, passwordToken } = req.body;
+
+    let userF = null;
+    User.find({
+        resetToken: password,
+        resetTokenExpiration: {
+            $gt: Date.now(),
+        },
+        _id: userId,
+    })
+        .then((user) => {
+            if (!user) {
+                return res.redirect('/');
+            }
+
+            userF = user;
+            return bcrypt.hash(password, 12);
+        })
+        .then((hashedPassword) => {
+            if (hashedPassword) {
+                userF.password = hashedPassword;
+                userF.resetToken = null;
+                userF.resetTokenExpiration = undefined;
+                return userF.save();
+            }
+        })
+        .then((user) => {
+            console.log(user);
+            res.redirect('/login');
         })
         .catch((error) => {
             console.log(error);
--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,9 @@ router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
+router.get('/reset-password/:token', authController.getResetPassword);
+router.post('/reset-password', authController.postResetPassword);
+router.get('/reset', authController.getReset);
+router.post('/reset', authController.postReset);
 
 module.exports = router;
