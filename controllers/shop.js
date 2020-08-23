903f62325d4acd1e366535ead717e543dab4cc63
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-08-23 12:11:22 -0400

    Adding pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,20 +5,43 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 // __dirname: get absolute path of the file where is used
 // path.join: concatenates files so it works on any OS  (do not use / (slashes))
 // "../" is allowed, to go up one level
 
 exports.getProducts = (req, res, next) => {
+    let { page } = req.query;
+    if (!page) page = 1;
+    else page = parseInt(page);
+
+    let total = null;
     Product.find()
+        .countDocuments()
+        .then((numProducts) => {
+            total = numProducts;
+
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE);
+        })
         .then((result) => {
-            // render using the template engine defined in "view engine" in the folder defined in "views"
-            // the second parameter is data that should be added to the template
-            res.render('shop/product-list', {
+            const data = {
                 path: '/products',
                 pageTitle: 'Shop',
                 prods: result,
-            });
+                currentPage: page,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(total / ITEMS_PER_PAGE),
+                hasNextPage: ITEMS_PER_PAGE * page < total,
+                hasPreviousPage: page > 1,
+            };
+
+            // render using the template engine defined in "view engine" in the folder defined in "views"
+            // the second parameter is data that should be added to the template
+            res.render('shop/product-list', data);
         })
         .catch((err) => {
             const error = new Error(err);
@@ -40,13 +63,35 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+    let { page } = req.query;
+    if (!page) page = 1;
+    else page = parseInt(page);
+
+    let total = null;
+
     Product.find()
+        .countDocuments()
+        .then((numProducts) => {
+            total = numProducts;
+
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE);
+        })
         .then((result) => {
-            res.render('shop/index', {
+            const data = {
                 path: '/',
                 pageTitle: 'Shop',
                 prods: result,
-            });
+                currentPage: page,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(total / ITEMS_PER_PAGE),
+                hasNextPage: ITEMS_PER_PAGE * page < total,
+                hasPreviousPage: page > 1,
+            };
+
+            res.render('shop/index', data);
         })
         .catch((err) => {
             const error = new Error(err);
