b7d52c2997b150d301aa029f261a3357ea8347e5
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-08-26 22:28:33 -0400

    Add project2 base

---
+++ p2-spa/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    // set all domains that could do request to server. * every domain
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // methods that client can request
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+
+    // headers that client can add to the request (there are default headers that are always allowed)
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+})
+app.use('/feed', feedRoutes);
+
+app.listen(8080);
