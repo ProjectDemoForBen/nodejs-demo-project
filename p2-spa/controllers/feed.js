a33dd199aef3f6ccde676e70ddc27da5c77ed808
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-08-26 23:44:48 -0400

    Add server side validation

--- p2-spa/controllers/feed.js
+++ p2-spa/controllers/feed.js
@@ -1,27 +1,46 @@
+const { validationResult } = require('express-validator');
 
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-       posts: [
-           {
-               title: 'First Post',
-               content: 'This is the first post',
-               author: 'Marcelo Cardozo',
-               imageUrl: '/images/2020-08-07_20-23.png',
-           }
-       ]
+        posts: [
+            {
+                id: 1,
+                title: 'First Post',
+                content: 'This is the first post',
+                creator: {
+                    name: 'Marcelo Cardozo',
+                },
+                imageUrl: '/images/2020-08-07_20-23.png',
+                createdAt: new Date().toISOString(),
+            }
+        ],
+        totalItems: 1,
     });
 }
 
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            message: 'Validation failed!',
+            errors: errors.array(),
+        })
+    }
+
+
     const {title, content} = req.body;
 
     res.status(201).json({
-       message: 'Post created!',
-       data: {
-           id: new Date().toISOString(),
-           title,
-           content,
-       }
+        message: 'Post created!',
+        post: {
+            id: new Date().toISOString(),
+            title,
+            content,
+            creator: {
+                name: 'Marcelo Cardozo',
+            },
+            createdAt: new Date().toISOString(),
+        }
     });
 }
--- p2-spa/routes/feed.js
+++ p2-spa/routes/feed.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
 router.get('/posts', feedController.getPosts);
-router.post('/posts', feedController.createPost);
+router.post('/posts', [
+    body('title', 'Title should have at least 5 characters').trim().isLength({min:5}),
+    body('content', 'Content should have at least 5 characters').trim().isLength({min:5}),
+],feedController.createPost);
 
 
 module.exports = router;
