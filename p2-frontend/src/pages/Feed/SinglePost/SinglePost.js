41df5bbdbcd734a16e4bbc5c8e786e05670e107c
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-08-28 19:09:52 -0400

    Get single post in frontend

--- p2-frontend/src/pages/Feed/SinglePost/SinglePost.js
+++ p2-frontend/src/pages/Feed/SinglePost/SinglePost.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Image from '../../../components/Image/Image';
 import './SinglePost.css';
+import config from "../../../config";
 
 class SinglePost extends Component {
   state = {
     title: '',
     author: '',
     date: '',
-    image: '',
+    imageUrl: '',
     content: ''
   };
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('URL')
+    const url = `${config.backend}/feed/posts/${postId}`
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch status');
@@ -26,7 +33,8 @@ class SinglePost extends Component {
           title: resData.post.title,
           author: resData.post.creator.name,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
-          content: resData.post.content
+          content: resData.post.content,
+          imageUrl: resData.post.imageUrl,
         });
       })
       .catch(err => {
@@ -42,7 +50,7 @@ class SinglePost extends Component {
           Created by {this.state.author} on {this.state.date}
         </h2>
         <div className="single-post__image">
-          <Image contain imageUrl={this.state.image} />
+          <Image contain imageUrl={this.state.imageUrl}/>
         </div>
         <p>{this.state.content}</p>
       </section>
@@ -51,3 +59,4 @@ class SinglePost extends Component {
 }
 
 export default SinglePost;
+
