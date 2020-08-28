c622c4faa92b374f551a07164713d77c20579f59
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-08-27 23:22:19 -0400

    Add user relationship

--- p2-spa/app.js
+++ p2-spa/app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const feedRoutes = require('./routes/feed');
+const sequelize = require('./utils/database');
+const Post = require('./models/post');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,9 +22,73 @@ app.use((req, res, next) => {
 
     next();
 })
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            next();
+        });
+})
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+User.hasMany(Post, {
+    foreignKey: {
+        name: 'userId',
+        allowNull: false
+    }
+});
+Post.belongsTo(User, {
+    as: 'creator',
+    constraints: true,
+    onDelete: 'CASCADE',
+    foreignKey: {
+        name: 'userId',
+        allowNull: false
+    }
+});
+
+
+sequelize
+    .sync({force: true})
+    .then(result => {
+        console.log(result);
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (user) {
+            return Promise.resolve(user);
+        } else {
+            return User.create({
+                name: 'Marcelo Cardozo',
+            })
+        }
+    })
+    .then(user => {
+        return user.getPosts()
+            .then(posts => {
+                console.log(posts);
+                if (posts.length === 0) {
+                    return user.createPost({
+                        title: 'Prueba',
+                        content: 'Content prueba',
+                        imageUrl: '/images/2020-08-07_20-23.png'
+                    });
+                } else {
+                    return Promise.resolve(posts[0]);
+                }
+            })
+    })
+    .then(post => {
+        app.listen(8080);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
