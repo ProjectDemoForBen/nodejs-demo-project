a3f945bd788c9b2d29bf0949ffc17c1aa7a1ad32
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-09-05 18:51:52 -0400

    Add values from html

--- p4-typescript/app.ts
+++ p4-typescript/app.ts
@@ -1,7 +1,19 @@
-function add(num1, num2) {
+// TS infers that num1 is HTMLElement
+const num1El = document.getElementById('num1') as HTMLInputElement;
+const num2El = document.getElementById('num2') as HTMLInputElement;
+const resultEl = document.getElementById('result') as HTMLParagraphElement;
+
+// TS infers that the button is of type HTMLButtonElement (because uses the selector)
+const button = document.querySelector('button');
+
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-console.log(add(1, 6));
+button.addEventListener('click', () => {
+  const num1 = parseInt(num1El.value);
+  const num2 = parseInt(num2El.value);
 
-console.log(add('1', '6'));
+  const result = add(num1, num2);
+  resultEl.innerText = `The result is ${result}`;
+});
