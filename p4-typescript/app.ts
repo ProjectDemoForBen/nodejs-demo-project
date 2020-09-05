9d728c5a7897c4c27586dd38134289f2d2b95675
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-09-05 19:02:23 -0400

    Add tsconfig

--- p4-typescript/app.ts
+++ p4-typescript/app.ts
@@ -4,7 +4,8 @@ const num2El = document.getElementById('num2') as HTMLInputElement;
 const resultEl = document.getElementById('result') as HTMLParagraphElement;
 
 // TS infers that the button is of type HTMLButtonElement (because uses the selector)
-const button = document.querySelector('button');
+// adding ! because we know that button will not be null
+const button = document.querySelector('button')!;
 
 function add(num1: number, num2: number) {
   return num1 + num2;
