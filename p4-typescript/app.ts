a3eb780d78113c7567c68607c6045b5faaeeb8c3
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-09-05 23:56:38 -0400

    Learning typescript basics

--- p4-typescript/app.ts
+++ p4-typescript/app.ts
@@ -7,14 +7,53 @@ const resultEl = document.getElementById('result') as HTMLParagraphElement;
 // adding ! because we know that button will not be null
 const button = document.querySelector('button')!;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
+const numberResults : Array<number> = [];
+const stringResults : string[] = [];
+
+type NumOrString = number | string;
+type Result = {val: number; timestamp: Date};
+
+interface ResultObj {
+  val: number;
+  timestamp: Date;
+}
+
+function add(num1: NumOrString, num2: NumOrString) {
+  if(typeof num1 === 'number' && typeof num2 === 'number')
+    return num1 + num2;
+  else if(typeof num1 === 'string' && typeof num2 === 'string')
+    return `${num1} ${num2}`;
+  else
+    return +num1 + +num2;
+}
+
+function printResult(obj : ResultObj) {
+  console.log(obj.val)
 }
 
 button.addEventListener('click', () => {
-  const num1 = parseInt(num1El.value);
-  const num2 = parseInt(num2El.value);
+  const num1 = num1El.value;
+  const num2 = num2El.value;
 
-  const result = add(num1, num2);
-  resultEl.innerText = `The result is ${result}`;
+  const numberResult = add(+num1, +num2);
+  numberResults.push(numberResult as number);
+
+  const stringResult = add(num1, num2);
+  stringResults.push(stringResult as string);
+
+  console.log(numberResults, stringResults);
+
+  resultEl.innerText = `The result is ${numberResult}`;
+  printResult({val: +num1, timestamp: new Date()})
 });
+
+const promise = new Promise<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve('resolved');
+  }, 1000);
+});
+
+promise.then(value => {
+  console.log(value.split('l'));
+});
+console.log('zzz');
